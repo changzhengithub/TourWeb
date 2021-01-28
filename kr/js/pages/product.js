f778f234a5d0dec244ec93818f5a40a05c6eac0c
changzhengithub <1979250385@qq.com>
2021-01-28 09:56:15 +0800

    优化韩文产品页

--- kr/js/pages/product.js
+++ kr/js/pages/product.js
@@ -5,10 +5,15 @@ new Vue({
 		isAndroid: false,
 		isIOS: false,
 		isWeixin: false,
+		playerFirst: null,
+		playerFourth: null,
 		openMenuShow: false,
 		openLanguageShow: false,
+		openBuyNowShow: false,
 		navList: [],
 		mySwiper: null,
+		swiperActiveIndex: 0,
+		swiperButtonList: ['정밀한 제조', '한 손으로 조작 가능', '안정적이고 믿을 만한 제품', '탁월한 촉감'],
 		navActiveIndex: 1,
 		playerShow: false,
 		ytVideoId: '',
@@ -16,44 +21,50 @@ new Vue({
 		fifthToastIconShow: false,
 		subTitle: '',
 		subText: '',
+		toastActiveIndex: 0,
 		toastVideoList: [
 			{
 				className: 'item-first',
 				itemText: '브러시 제어',
-				subTitle: '효율적이고 정확한 브러시 제어',
-				subText: '노브를 사용하여 <span>브러시의 크기, 흐름, 불투명도 및 경도를</span> 쉽게 제어 할 수 있으므로, 작업의 흐름을 중단하지 않고 창작에 집중할 수 있습니다.',
+				icon: 'iconBrushcontrol',
+				subTitle: '정확하고 효율적인 브러시 제어',
+				subText: '노브로 브러시 크기, 흐름, 불투명도, 강도를 손쉽게 제어할 수 있고 하던 작업을 중단시키지 않으며 창작에 집중할 수 있도록 합니다 .',
 				videoUrl: '//r.tourboxtech.com/kr/src/videos/product-second-more-01-2.0.2.mp4',
 				posterUrl: ''
 			},
 			{
 				className: 'item-second',
-				itemText: '파라미터 제어',
-				subTitle: '강력하고 다양한 파라미터 제어',
-				subText: '<span>슬라이더 제어, 수치 제어,프레임 별 스텝핑, 또는 빠른 검색</span> 등 조작에, TourBox의 다이얼을 사용하면 모든 파라미터를 정밀하게 제어 할 수 있으므로, 이제 마우스 드래그와  헤어질 때가 되었습니다.',
+				itemText: '파라미터 조절',
+				icon: 'iconParametercontrol',
+				subTitle: '강력하고 다양한 파라미터 조절 ',
+				subText: 'TourBox 다이얼 하나만으로  슬라이드 제어, 수치 조절, 프레임 속도 조정 또는 빠른 검색 등 작업들은 물론이고 모든 파라미터 값도 정밀하게 조절할 수 있습니다. 더 이상 ',
 				videoUrl: '//r.tourboxtech.com/kr/src/videos/product-second-more-02-2.0.2.mp4',
 				posterUrl: ''
 			},
 			{
 				className: 'item-third',
-				itemText: '비디오 및 오디오 편집',
-				subTitle: '비디오 및 오디오 편집',
-				subText: '<span>다이얼, 노브 및 스크롤은</span> 다양한 버튼들과 함께 사용하며, 보다 직관적이고 효율적인 비디오 및 오디오 편집을 구현할 수 있습니다.',
+				itemText: '비디어 및 오디오 편집',
+				icon: 'iconVideoandaudioediting',
+				subTitle: '비디어 및 오디오 편집',
+				subText: '다이얼, 노브 및 스크롤을 다양한 버튼 들과 함께 사용하시면 비디어 및 오디어 편집 작업은 보다 직관적이고 효율성도 크게 높일 수 있습니다.',
 				videoUrl: '//r.tourboxtech.com/kr/src/videos/product-second-more-03-2.0.2.mp4',
 				posterUrl: ''
 			},
 			{
 				className: 'item-fourth',
-				itemText: '이미지 제어',
-				subTitle: '유창하고 원활한 탐색 작업',
-				subText: '<span>이미지 확대 / 축소, 캔버스 회전, 타임 라인 확대 / 축소</span> 등과 같은 일련의 탐색 작업에서 <span>스크롤, 노브 혹은 다이얼</span>을 사용하면，소프트웨어를 보다 직관적이고 효율적이며 정확한 방식으로 컨트롤할 수 있고, 또한 <span>사이드 키 ,톨 버튼, 쇼트  버튼,</span> 및 기타 버튼을 함께 사용하여 보다 고급 기능을 수행 할 수 있습니다.',
+				itemText: '화면 제어',
+				icon: 'iconImagecontrol',
+				subTitle: '유창하고 원활해지는 편집 작업',
+				subText: '이미지 확대/축소, 캔버스 회전, 타임라인 확대/축소 등 일련의 편집 작업을 진행하실 때 TourBox의 스크롤, 노브, 다이얼, 이 3까지 버튼과 사이드 버튼, 톨 버튼, 쇼트.',
 				videoUrl: '//r.tourboxtech.com/kr/src/videos/product-second-more-04-2.0.2.mp4',
 				posterUrl: ''
 			},
 			{
 				className: 'item-fifth',
 				itemText: '도구 전환',
-				subTitle: '빠르고 쉬운 도구 전환',
-				subText: '십자패드를 사용하면 여러 도구 세트 간의 <span>빠르고 정확한 전환을 구현</span>할 수 있으며, HUD 기능을 통해 실시간으로 확인할 수 있습니다.',
+				icon: 'iconToolswitching',
+				subTitle: '보다 빠르고 손쉬운 도구 전환',
+				subText: '십자패드를 통하여 여러 세트 간의 호환은 빠르고 정확할 뿐만 아니라 HUD 기능으로 실시간 확인도 가능합니다 .',
 				videoUrl: '//r.tourboxtech.com/kr/src/videos/product-second-more-05-2.0.2.mp4',
 				posterUrl: ''
 			}
@@ -68,8 +79,8 @@ new Vue({
 	mounted: function () {
 		var that = this;
 		that.$nextTick(function () {
-			that.listenerMobileVideo();
 			that.initSwiper();
+			that.listenerMobileVideo();
 		})
 	},
 	methods: {
@@ -91,9 +102,17 @@ new Vue({
 			that.openMenuShow = false;
 			$(".header-mobile .language-select").slideToggle("fast");
 		},
-		checkLanguage:function(lan) {
+
+		openBuyNow: function(){
+			var that = this;
+			if(!that.isM) return
+			that.openBuyNowShow = !that.openBuyNowShow;
+			$(".header-buy .buy-list").toggleClass('list-active', that.openBuyNowShow)
+		},
+		
+    checkLanguage:function(lan) {
 			var _this = this;
-			var localUrl = window.location.href;
+      var localUrl = window.location.href;
 			if (localUrl.indexOf('kr/') == -1) return
 			var arr1=localUrl.split('kr/');
 			if (arr1[1]) {
@@ -107,33 +126,39 @@ new Vue({
 		// 加载头部swiper轮播图
 		initSwiper: function () {
 			var that = this;
-			setTimeout(function() {
-				that.mySwiper = new Swiper('.swiper-container', {
-					autoplay: {
-						delay: 2500,
-						disableOnInteraction: true,
+			that.mySwiper = new Swiper('.swiper-container', {
+				autoplay: {
+					delay: 2500,
+					disableOnInteraction: true,
+				},
+				loop : true,
+				effect : that.isM ? 'slide' : 'fade',
+				// on:{
+				// 	slideChangeTransitionStart: function(){
+				// 		that.swiperActiveIndex = this.realIndex;
+				// 	},
+				// },
+				pagination: {
+					el: '.swiper-pagination',
+					clickable: true,
+					renderBullet: function (index, className) {
+						if (that.isM) {
+							return '<span class="' + className + '"></span>'
+						} else {
+							return '<span class="' + className + '">' + that.swiperButtonList[index] + '</span>';
+						}
 					},
-					loop : that.isM,
-					effect : that.isM ? 'bullets' : 'fade',
-					fadeEffect: {
-						crossFade: true,
-					},
-					noSwiping : true,
-					pagination: {
-						el: '.swiper-pagination',
-						clickable: true,
-						renderBullet: function (index, className) {
-							if (that.isM) {
-								return '<span class="' + className + '"></span>'
-							} else {
-								return '<span class="' + className + '">' + (index + 1) + '</span>';
-							}
-						},
-					},
-					observer: true,
-					observeParents: true,
-				});
-			}, 1000)
+				},
+				observer: true,
+				observeParents: true,
+			});
+		},
+		switchSwiper: function(index) {
+			var that = this;
+			if (that.swiperActiveIndex == index) return
+			if (!that.mySwiper) return
+			that.swiperActiveIndex = index;
+			that.mySwiper.slideTo(index, 1000, false);
 		},
 
 		openFixedVideo: function(url) {
@@ -180,8 +205,16 @@ new Vue({
 			$(event.currentTarget).parent().next().next().addClass('play-hide');
 		},
 
+		switchToast: function(index) {
+			var that = this;
+			if (that.toastActiveIndex === index) return
+			that.toastActiveIndex = index;
+			that.fifthToastIconShow = false
+		},
+
 		openSubToast: function(index) {
 			var that = this;
+			if (that.isM) return
 			that.subToastShow = true
 			$('body').css({
 				overflow: 'hidden'
@@ -276,5 +309,6 @@ new Vue({
 			that.isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);     //判断是否是 iOS终端
 			if (u.toLowerCase().match(/MicroMessenger/i) == 'micromessenger') that.isWeixin = true; // 判断是否是微信
 		},
+
 	}
 })
